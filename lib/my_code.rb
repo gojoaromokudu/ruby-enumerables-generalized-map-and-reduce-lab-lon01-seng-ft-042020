2d7db0d64bbd504840df93d1587e4fc11e1afe66
Gbenga Ojo-Aromokudu <gojoaromokudu@gmail.com>
2020-03-23 22:17:16 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,20 @@
 # Your Code Here
+def map (array)
+new_array = []
+index = 0 
+while index < array.length do 
+  new_array.push(yield(array[index]))
+  index += 1 
+end
+new_array
+end
+
+def reduce (array, start = 0)
+  total = start
+  index = 0
+  while index < array.length do 
+    total = yield(total, array[index])
+    index += 1
+  end
+  total
+end
